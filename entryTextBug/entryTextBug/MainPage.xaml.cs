4631346aabab2831e9b9ea78d3aa81de1c16c0a1
AmrH <AmrH@DEV-AMR>
2020-02-23 16:32:20 +0200

    Add project files.

---
+++ entryTextBug/entryTextBug/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace entryTextBug
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<string> items = new List<string>() { "Male","Female" };
+          
+
+            GenderPicker.ItemsSource = items;
+            GenderPicker.SelectedItem = items[0];
+            GenderPicker.SelectedIndex = 0;
+        }
+
+        private void ChangeLangugeClicked(object sender, EventArgs e)
+        {
+
+            App.Current.MainPage = new NavigationPage(new MainPageArabic());
+
+        }
+    }
+}
---
+++ entryTextBug/entryTextBug/MainPageArabic.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace entryTextBug
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageArabic : ContentPage
+    {
+        public MainPageArabic()
+        {
+            InitializeComponent();         
+            List<string> items = new List<string>() { "ذكر", "انثى" };
+            GenderPicker.ItemsSource = items;
+            GenderPicker.SelectedItem = items[0];
+            GenderPicker.SelectedIndex = 0;
+
+        }
+        private void ChangeLangugeClicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
